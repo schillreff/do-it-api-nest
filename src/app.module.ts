e8f90b53fe706a6a1e1d48c4f63ae14215e2761b
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-18 14:17:30 -0400

    feat: configure application with global settings and PostgreSQL database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,36 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { Note } from './notes/entities/note.entity';
+import { NotesModule } from './notes/notes.module';
+import { User } from './users/entities/user.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        username: config.get('DB_USERNAME'),
+        password: config.get('DB_PASSWORD'),
+        database: config.get('DB_DATABASE'),
+        entities: [User, Note],
+        synchronize: config.get('DB_SYNCHRONIZE'),
+      }),
+    }),
+
+    UsersModule,
+    NotesModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
